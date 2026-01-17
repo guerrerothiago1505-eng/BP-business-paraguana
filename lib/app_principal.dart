f77129b0dfeaa912441315246ee1960b3999060d
guerrerothiago1505-eng <guerrerothiago1505@gmail.com>
2026-01-17 13:25:22 -0400

    Update app_principal.dart

--- lib/app_principal.dart
+++ lib/app_principal.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
